ce3fb3508488fb8e79fd7227d8db4dc542bc98ef
Arun <121198022+arun-spar@users.noreply.github.com>
2024-09-27 09:28:37 -0400

    Update CustomNameIdGenerator.cs

--- src/MyIDP/CustomNameIdGenerator.cs
+++ src/MyIDP/CustomNameIdGenerator.cs
@@ -11,29 +11,41 @@ using Rsk.Saml.Generators;
 
 namespace IdentityServer
 {
-   
-        public class CustomNameIdGenerator : SamlNameIdGenerator
+
+    public class CustomNameIdGenerator : SamlNameIdGenerator
     {
         private readonly ILogger<ISamlNameIdGenerator> _logger;
         public CustomNameIdGenerator(ILogger<ISamlNameIdGenerator> logger) : base(logger)
         {
-            _logger=logger;
-           
+            _logger = logger;
+
         }
 
-        public override async Task<Saml2Subject> GenerateNameId(string subjectId, IList<Claim> userClaims, string defaultFormat, string requestedFormat )
+        public override async Task<Saml2Subject> GenerateNameId(string subjectId, IList<Claim> userClaims, string defaultFormat, string requestedFormat)
         {
             const string customFormat = SamlConstants.NameIdentifierFormats.Transient;
 
-            if (requestedFormat == customFormat)
+            /*  if (requestedFormat == customFormat)
+             {
+                 var nameIdValue = userClaims.FirstOrDefault(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/SURID")?.Value;
+                 _logger.LogInformation("nameIdValue: {nameIdValue}",nameIdValue);
+                 return new Saml2Subject(new Saml2NameIdentifier(nameIdValue, new Uri(customFormat)));
+             }
+
+             return await base.GenerateNameId(subjectId, userClaims, defaultFormat, requestedFormat); */
+
+            var responseFormat = GetResponseFormat(requestedFormat, defaultFormat);
+            if (responseFormat == SamlConstants.NameIdentifierFormats.Transient)
             {
-                var nameIdValue = userClaims.FirstOrDefault(x => x.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/SURID")?.Value;
-                _logger.LogInformation("nameIdValue: {nameIdValue}",nameIdValue);
-                return new Saml2Subject(new Saml2NameIdentifier(nameIdValue, new Uri(customFormat)));
+                var nameIdValue = userClaims.FirstOrDefault(x => x.Type == "SURID")?.Value;
+                _logger.LogInformation("nameIdValue: {nameIdValue}", nameIdValue);
+                return new Saml2Subject(new Saml2NameIdentifier(nameIdValue, new Uri(SamlConstants.NameIdentifierFormats.Transient)));
             }
 
             return await base.GenerateNameId(subjectId, userClaims, defaultFormat, requestedFormat);
+
+
         }
     }
-    
+
 }
